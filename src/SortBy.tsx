9793a0cf3bb8d430f23bd690dddadd400485d666
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-03-08 21:42:37 +0000

    added select return in SortBy component, and added onChange function that sets sortBy to selected value

--- src/SortBy.tsx
+++ src/SortBy.tsx
@@ -5,7 +5,19 @@ interface SortByProps {
 }
 
 function SortBy ({setSortBy}: SortByProps) {
-    return (<></>)
+    const [selected, setSelected] = useState("created at")
+    const handleSortBy = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const newSelection = e.target.value
+        setSelected(newSelection)
+    }
+
+    selected === "created at" ? setSortBy("created at") : setSortBy("due date")
+
+    return (
+    <select value={selected} onChange={handleSortBy}>
+        <option value="created at">created at</option>
+        <option value="due date">due date</option>
+    </select>)
 }
 
 export default SortBy
