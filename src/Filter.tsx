ef7f4c278388b3f186690170dcef5f88c2ac169d
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-03-08 21:24:12 +0000

    updated Filter component to return a select component with an option for all tasks and another option for completed task. Filter state is set to the selected option

--- src/Filter.tsx
+++ src/Filter.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 interface FilterProps {
   setFilter: React.Dispatch<React.SetStateAction<string>>;
 }
 
 function Filter ({setFilter}: FilterProps) {
+    const [selectedFilter, setSelectedFilter] = useState("all tasks")
+
+    const handleFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const newSelection = e.target.value
+        setSelectedFilter(newSelection)
+        newSelection === "completed" ? setFilter("completed=true") : setFilter("completed=false")
+        console.log("new selection in handle filter", newSelection)
+        console.log("setFilter in handle filter", setFilter)
+    }
+
     return (
-        <></>
+        <select value={selectedFilter} onChange={handleFilter}>
+            <option value="all tasks">all tasks</option>
+            <option value="completed">completed</option>
+        </select>
     )
 }
 
